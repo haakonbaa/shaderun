f5b81ef974a13a8d05a48a4b51ff3bcbf8098968
Håkon Bårsaune <haakonbaa@gmail.com>
2024-08-18 21:25:01 +0200

    Cool fragment shader

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -1,44 +1,33 @@
 #version 330 core
 in vec3 texCoord;
-in vec2 test;
 out vec4 fragColor;
 uniform float uTime;
 
-float dot2( in vec2 v ) { return dot(v,v); }
-
-float sdHeart( in vec2 p )
+vec3 palette( in float t)
 {
-    p.x = abs(p.x);
-
-    if( p.y+p.x>1.0 )
-        return sqrt(dot2(p-vec2(0.25,0.75))) - sqrt(2.0)/4.0;
-    return sqrt(min(dot2(p-vec2(0.00,1.00)),
-                    dot2(p-0.5*max(p.x+p.y,0.0)))) * sign(p.x-p.y);
+    vec3 a = vec3(0.5,0.5,0.5);
+    vec3 b = vec3(0.5,0.5,0.5);
+    vec3 c = vec3(1.0,1.0,1.0);
+    vec3 d = vec3(0.0,0.33,0.67);
+    return a + b*cos( 6.28318*(c*t+d) );
 }
 
 void main()
 {
-    //mat2x2 Ry = mat2x2(1/cos(0*uTime), 0.0, 0.0, 1.0);
-    //mat2x2 Rx = mat2x2(1.0, 0.0, 0.0, 1/cos(uTime));
-    float ay = 1.5*uTime;
-    float ax = 1.0*uTime;
-    mat2x2 Ryx = mat2x2(cos(ay), sin(ay), -sin(ax)*sin(ay), cos(ax)-sin(ax)*cos(ay));
-    vec3 pos = vec3( inverse(Ryx)*texCoord.xy, 0.0);
-    /*
-    if (length(pos.xy) > 0.5) {
-        fragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    } else {
-        fragColor = vec4(0.0, 1.0, 0.0, 1.0);
-    }
+    vec2 pos = texCoord.xy;
+    vec3 finalColor = vec3(0.0);
 
-    return;
-    */
-    float d = 0.05/abs(0.5-length(pos.xy));
-    d = 1.0-sdHeart(pos.xy+vec2(0.0, 0.5));
-    fragColor = vec4(d, 0.0, 0.0, 1.0);
-    if (d >= 1.0) {
-        float gb = 0.3;
-        fragColor = vec4(1.0, gb, gb, 1.0);
+    float d;
+    vec3 col;
+    for (float i = 0.0; i < 3.0; i++) {
+        pos = 1.5*(fract(1.5*pos)-0.5);
+        col = palette(length(texCoord.xy) + i*0.5 + 0.5*uTime);
+        d = length(pos)*exp(-length(texCoord.xy));
+        d = sin(14.0*d + 3.0*uTime);
+        d = 0.5*(d + 1.0);
+        d = 0.1/d;
+        d = pow(d, 1.2);
+        finalColor += d*col/(4.0*i+1.0);
     }
-    //fragColor = vec4(0.5*(sin(uTime) + 1), r, g, 1.0);
+    fragColor = vec4(finalColor, 1.0);
 }
--- vertex_shader.glsl
+++ vertex_shader.glsl
@@ -1,11 +1,8 @@
 #version 330 core
 layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec3 aColor;
-out vec2 test;
 out vec3 texCoord;
 void main()
 {
     gl_Position = vec4(aPos, 1.0);
     texCoord = vec3(aPos.xy, 0.0);
-    test = vec2(0.0, 1.0);
 }
